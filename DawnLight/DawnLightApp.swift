81f36f0d919cc35ba1f82bf6b7755b372e938068
Václavík Martin <mvaclavik96@icloud.com>
2021-01-18 13:07:56 +0100

    Initial Commit

---
+++ DawnLight/DawnLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  DawnLightApp.swift
+//  DawnLight
+//
+//  Created by Martin Václavík on 18.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct DawnLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
